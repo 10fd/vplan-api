bd5bf3fc691046c0aaef91171edb8c55e8e19c51
10fd <finnegan.adryan@protonmail.com>
2021-10-05 23:04:48 +0200

    ueberhaupt erstmal die dateien

---
+++ index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import json
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return jsonify({'name': 'alice',
+                    'email': 'alice@outlook.com'})
+
+app.run()
