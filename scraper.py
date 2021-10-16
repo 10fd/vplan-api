a3fe1d4f6e2fb9f4056a57b99506514707d493b8
10fd <finnegan.adryan@protonmail.com>
2021-10-16 02:15:14 +0200

    regex fehler so halb behoben

--- scraper.py
+++ scraper.py
@@ -2,7 +2,6 @@
 # encoding: utf-8
 import regex
 import requests
-#from bs4 import BeautifulSoup
 
 url_heute = 'http://extra.taunusgymnasium.de/vplan/f2/subst_001.htm'
 url_morgen = 'http://extra.taunusgymnasium.de/vplan/f2/subst_001.htm'
@@ -15,14 +14,14 @@ def getData(content):
     column = 0
     plan = {}
 
-    for line in content:
-        result = regex.search(r"<tr class='list (odd|even)'>", line)
+    for str in content.splitlines():
+        result = regex.search(r"<tr class='list (odd|even)'>", str)
         if result:
             recordno += 1
             column = -1
             plan[recordno] = {}
     
-        result = regex.search("color: #010101\">(.*)<", line)
+        result = regex.search(r"color: #010101\">(.{1,7})<", str)
         if result:
             column += 1
             plan[recordno][column] = result.groups(0)
@@ -31,9 +30,6 @@ def getData(content):
 
     for record in plan.items():
         print(record)
-
-#webContent_soup = BeautifulSoup(getWebContent(url_heute), 'html.parser')
-#print(webContent_soup)
 
 getData(getWebContent(url_heute))
 getData(getWebContent(url_morgen))
